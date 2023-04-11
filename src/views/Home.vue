d761215e5680ac638770b0edfedf72a9b4ef26ea
ouchen <medouchen19@gmail.com>
2023-04-11 13:08:33 +0200

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <HelloWorld />
+  <v-card text="textxcycycuhvchcveh" variant="tonal"></v-card>
+</template>
+
+<script lang="ts" setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
